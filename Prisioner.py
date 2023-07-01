833fee1ff955172a211f6aa03e0884ecb61394f7
brenno-silva <brenno.silva037@gmail.com>
2023-07-01 16:58:52 -0300

    Adicionando lógica inicial do PrisonerAgent
    Co-authored-by: RafaelBP02 <RafaelBP02@users.noreply.github.com>
    Co-authored-by: matheuscvp <matheuscvp12@gmail.com>

--- Prisioner.py
+++ Prisioner.py
@@ -1,23 +1,63 @@
 import random
 from mesa import Model, Agent
 from mesa.time import RandomActivation
-from mesa.space import MultiGrid
+from mesa.space import SingleGrid, MultiGrid
+
+GRID_WIDTH = 30
+GRID_HEIGHT = 30
+
+def prisioner_dillema():
+    probability = 0.5
+    return random.random() < probability
 
 
 class PrisonerAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
+        self.jail_time = 5
+        self.is_informer = False
+        self.denounced_list = []
 
     def step(self):
         self.move()
+        self.snitch()
 
     def move(self):
         possible_steps = self.model.grid.get_neighborhood(
-            self.pos, moore=False, include_center=True
-        )
-
+            self.pos,
+            moore=True,  # Verifica as diagonais
+            include_center=True)
         new_position = self.random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
+
+    def snitch(self):
+        adjacent_positions = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,  # Verifica as diagonais
+            include_center=False)
+
+        adjacent_agents = []
+        for pos in adjacent_positions:
+            x, y = pos
+            if 0 <= x < GRID_WIDTH and 0 <= y < GRID_HEIGHT:  # Verifica se a posição é válida na grade
+                agent = self.model.grid[x][y]  # Obtém o agente na posição (x, y) da grade
+                if agent:
+                    adjacent_agents.append(agent[0])
+
+        if adjacent_agents:
+            neighbor = self.random.choice(adjacent_agents)
+
+            should_snitch = prisioner_dillema()
+            if should_snitch:
+                self.is_informer = True
+                self.denounced_list.append(neighbor)
+                if self in neighbor.denounced_list:
+                    self.jail_time = self.jail_time/2
+                else:
+                    self.jail_time -= 1
+            else:
+                if self in neighbor.denounced_list:
+                    self.jail_time = self.jail_time*2
 
 
 class PrisonerModel(Model):
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Prisioner import PrisonerModel
+from Prisioner import PrisonerModel, GRID_WIDTH, GRID_HEIGHT
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import ChartModule
@@ -6,19 +6,24 @@ from mesa.visualization.modules import ChartModule
 
 def agent_portrayal(agent):
     portrayal = {"Shape": "circle", "Filled": "true", "r": 1}
-    portrayal["Color"] = "red"
+
+    if agent.is_informer:
+        portrayal["Color"] = "red"
+    else:
+        portrayal["Color"] = "blue"
+
     portrayal["Layer"] = 0
 
     return portrayal
 
 
-grid = CanvasGrid(agent_portrayal, 30, 30, 600, 600)
+grid = CanvasGrid(agent_portrayal, GRID_WIDTH, GRID_HEIGHT, 600, 600)
 
 server = ModularServer(
     PrisonerModel,
     [grid],
     "Prisioner Model",
-    {"num_agents": 100, "width": 30, "height": 30},
+    {"num_agents": 10, "width": 30, "height": 30},
 )
 server.port = 8521
 server.launch()
