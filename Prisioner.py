9f35361d3f349a11ebf9228a1fc2ec11b026630d
brenno-silva <brenno.silva037@gmail.com>
2023-07-01 19:27:12 -0300

    Adicionando tipos de prisioneiros
    Co-authored-by: RafaelBP02 <RafaelBP02@users.noreply.github.com>
        Co-authored-by: matheuscvp <matheuscvp12@gmail.com>

--- Prisioner.py
+++ Prisioner.py
@@ -6,17 +6,21 @@ from mesa.space import SingleGrid, MultiGrid
 GRID_WIDTH = 30
 GRID_HEIGHT = 30
 
-def prisioner_dillema():
-    probability = 0.5
-    return random.random() < probability
+PRISIONERS_PER_TYPE = 5
+
+PRISIONERS_TYPES = {
+    "AUTRUISTA": 1 , #não delata
+    "EGOISTA": 2, #sempre delata
+    "FLEXIVEL": 3, #depende da descisão do vizinho
+}
 
 
 class PrisonerAgent(Agent):
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id, model, type):
         super().__init__(unique_id, model)
-        self.jail_time = 5
-        self.is_informer = False
+        self.jail_time = random.randrange(1, 10)
         self.denounced_list = []
+        self.type = type
 
     def step(self):
         self.move()
@@ -31,33 +35,56 @@ class PrisonerAgent(Agent):
         self.model.grid.move_agent(self, new_position)
 
     def snitch(self):
+        # Pega posições em volta
         adjacent_positions = self.model.grid.get_neighborhood(
             self.pos,
             moore=True,  # Verifica as diagonais
             include_center=False)
 
+        # Retorna agentes próximos, em volta
         adjacent_agents = []
         for pos in adjacent_positions:
             x, y = pos
             if 0 <= x < GRID_WIDTH and 0 <= y < GRID_HEIGHT:  # Verifica se a posição é válida na grade
                 agent = self.model.grid[x][y]  # Obtém o agente na posição (x, y) da grade
                 if agent:
-                    adjacent_agents.append(agent[0])
+                    adjacent_agents.append(agent)
 
-        if adjacent_agents:
-            neighbor = self.random.choice(adjacent_agents)
+        if len(adjacent_agents) == 0:
+            return
+        
+        best_jail_time = self.jail_time
+        best_type = self.type
+        best_neighbor = self.random.choice(adjacent_agents)[0]
 
-            should_snitch = prisioner_dillema()
-            if should_snitch:
-                self.is_informer = True
-                self.denounced_list.append(neighbor)
-                if self in neighbor.denounced_list:
-                    self.jail_time = self.jail_time/2
-                else:
-                    self.jail_time -= 1
+        for neighbor_list in adjacent_agents:
+            for neighbor in neighbor_list:
+                if neighbor.jail_time < best_jail_time:
+                    best_type = neighbor.type
+                    best_jail_time = neighbor.jail_time
+                    best_neighbor = neighbor
+
+        if best_type != self.type:
+            self.type = best_type
+
+        # jail_time eh referente ao tempo de cadeida. Esta em anos
+        if PRISIONERS_TYPES[self.type] == PRISIONERS_TYPES["EGOISTA"]:
+            self.denounced_list.append(best_neighbor)
+            if self in best_neighbor.denounced_list:
+                self.jail_time = self.jail_time + 5
             else:
-                if self in neighbor.denounced_list:
-                    self.jail_time = self.jail_time*2
+                self.jail_time = self.jail_time 
+        elif PRISIONERS_TYPES[self.type] == PRISIONERS_TYPES["FLEXIVEL"]: # imita o melhor visinho
+            if self in best_neighbor.denounced_list:
+                self.denounced_list.append(best_neighbor)
+                self.jail_time = self.jail_time + 5
+            else:
+                self.jail_time = self.jail_time + 0.5
+        else: # caso do comportamento ser AUTRUISTA
+            if self in best_neighbor.denounced_list:
+                self.jail_time = self.jail_time + 10
+            else:
+                self.jail_time = self.jail_time + 0.5
 
 
 class PrisonerModel(Model):
@@ -66,13 +93,16 @@ class PrisonerModel(Model):
         self.schedule = RandomActivation(self)
         self.grid = MultiGrid(width, height, False)
         self.running = True
-
-        for i in range(self.num_agents):
-            a = PrisonerAgent(i, self)
-            self.schedule.add(a)
-            x = self.random.randrange(self.grid.width)
-            y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(a, (x, y))
+        
+        index = 0
+        for type in PRISIONERS_TYPES:
+            for i in range(PRISIONERS_PER_TYPE): 
+                a = PrisonerAgent(index, self, type) 
+                self.schedule.add(a)
+                x = self.random.randrange(self.grid.width)
+                y = self.random.randrange(self.grid.height)
+                self.grid.place_agent(a, (x, y))
+                index += 1
 
     def step(self):
-        self.schedule.step()
+        self.schedule.step() 
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from Prisioner import PrisonerModel, GRID_WIDTH, GRID_HEIGHT
+from Prisioner import PrisonerModel, GRID_WIDTH, GRID_HEIGHT, PRISIONERS_TYPES
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import ChartModule
 
-
 def agent_portrayal(agent):
     portrayal = {"Shape": "circle", "Filled": "true", "r": 1}
 
-    if agent.is_informer:
+    
+    if PRISIONERS_TYPES[agent.type] == PRISIONERS_TYPES["AUTRUISTA"]:
+        portrayal["Color"] = "green"
+    elif PRISIONERS_TYPES[agent.type] == PRISIONERS_TYPES["EGOISTA"]:
         portrayal["Color"] = "red"
     else:
         portrayal["Color"] = "blue"
-
+        
     portrayal["Layer"] = 0
 
     return portrayal
@@ -23,7 +25,7 @@ server = ModularServer(
     PrisonerModel,
     [grid],
     "Prisioner Model",
-    {"num_agents": 10, "width": 30, "height": 30},
+    {"num_agents": 15, "width": 30, "height": 30},
 )
 server.port = 8521
 server.launch()
